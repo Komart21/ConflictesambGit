48e0a6fd2176866e8bbbda0582391c6e3850af42
«Komart21» <usuari@mpesserradilla.cf@iesesteveterradas.cat>
2024-09-16 17:00:10 +0200

    PI mes exacte

--- circ.py
+++ circ.py
@@ -1,6 +1,10 @@
 # Arxiu circ.py : càlcul perímetre d'una circumferència
  
 radi = input("Radi de la circumferència? ")
+<<<<<<< HEAD
 PI = 3.1416
+=======
+PI = 3.14159
+>>>>>>> 12b153f (PI mes exacte)
 perimetre = 2*PI*float(radi)
 print("El perímetre de la circumferència de radi {} és {}".format(radi,perimetre))
